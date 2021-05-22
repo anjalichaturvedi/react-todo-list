9ad5855441b3aa2e2e66c2408269f787c9393600
Anjali Chaturvedi <anjaliichaturvedi@gmail.com>
2021-05-22 21:37:14 +0530

    added main components

---
+++ todo-list/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import DateTime from './components/DateTime.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        < DateTime/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
